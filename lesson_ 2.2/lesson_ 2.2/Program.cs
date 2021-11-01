9477e7ec598e0eb891a3646cc5689ed717f4f6b1
noncombatant1 <dubovetsaleksey@gmail.com>
2021-11-01 13:49:06 +0600

    created

---
+++ lesson_ 2.2/lesson_ 2.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson__2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер текущего месяца");
+            int numberofmounth = Convert.ToInt32(Console.ReadLine());
+            string mouthTitle = "";
+
+            switch (numberofmounth)
+            {
+                case 1:
+                    mouthTitle = "Январь";
+                    break;
+                case 2:
+                    mouthTitle = "Февраль";
+                    break;
+                case 3:
+                    mouthTitle = "Март";
+                    break;
+                case 4:
+                    mouthTitle = "Апрель";
+                    break;
+                case 5:
+                    mouthTitle = "Май";
+                    break;
+                case 6:
+                    mouthTitle = "Июнь";
+                    break;
+                case 7:
+                    mouthTitle = "Июль";
+                    break;
+                case 8:
+                    mouthTitle = "Август";
+                    break;
+                case 9:
+                    mouthTitle = "Сентябрь";
+                    break;
+                case 10:
+                    mouthTitle = "Октябрь";
+                    break;
+                case 11:
+                    mouthTitle = "Ноябрь";
+                    break;
+                case 12:
+                    mouthTitle = "Декабрь";
+                    break;
+            }
+            Console.WriteLine("Текущий месяц - " + mouthTitle);
+        }
+    }
+}
---
+++ lesson_2.1/lesson_2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            double maxtemperature = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            double mintemperature = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Средняя температура = " + ((maxtemperature + mintemperature) / 2));
+
+            Console.ReadKey();
+        }
+    }
+}
